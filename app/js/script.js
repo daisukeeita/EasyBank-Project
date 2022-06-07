218f0e07ec5a35c09303d7e5f9a5e142e35342f4
Acolyptos <vinas.johnandrei@gmail.com>
2022-06-07 22:21:38 +0800

    adding responsive header and bit of hero content in mobile device

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log(`Hello World!`);
+const hamburgerContainer = document.querySelector('#hamburger-container');
+const headerHamburger = document.querySelector('.header__hamburger');
+const header = document.querySelector('.header');
+
+hamburgerContainer.addEventListener('click', () => {
+    header.classList.toggle('open');
+} )
+
+console.log(`Hello, My old idiot friend!`);
